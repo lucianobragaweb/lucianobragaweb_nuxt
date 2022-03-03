3ab615650d60e3476588561095b52e8c4e4a928e
Luciano Braga <F001408@modal.net.br>
2022-03-03 18:39:17 -0300

    Updates

--- _nuxt/static/1646343164/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646343164",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"101d4383"}}}}(null,"\u002F"));
