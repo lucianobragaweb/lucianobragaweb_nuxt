916de1e022d2b05ab85a48ad0c0757768c522d7f
Luciano Braga <F001408@modal.net.br>
2022-03-04 11:24:41 -0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,20 @@
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/lucianobragaweb.github.io/'
+        }
+      }
+    : {}
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  router: {
-    base: '/lucianobragaweb.github.io/'
-  },
+  mode: 'universal',
+  ...routerBase,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'lucianobragaweb',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
